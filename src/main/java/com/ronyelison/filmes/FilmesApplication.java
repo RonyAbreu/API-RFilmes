01ce5f0669caa598c5237d749236002d2d4d714f
RonyAbreu <ronyelison78@gmail.com>
2024-01-04 19:32:59 -0300

    Correction: Corrigindo rota de home

--- src/main/java/com/ronyelison/filmes/FilmesApplication.java
+++ src/main/java/com/ronyelison/filmes/FilmesApplication.java
@@ -2,12 +2,22 @@ package com.ronyelison.filmes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class FilmesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	@ResponseBody
+	public String bemVindo(){
+		return "Bem vindo a API RFilmes!";
 	}
 
 }
