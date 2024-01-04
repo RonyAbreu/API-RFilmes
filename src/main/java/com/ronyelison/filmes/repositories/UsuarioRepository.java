8af1e0b0391f4a8dee9c6c9a34f235066d09067b
RonyAbreu <ronyelison78@gmail.com>
2024-01-04 19:21:46 -0300

    Create: Criação do UsuarioRepository

---
+++ src/main/java/com/ronyelison/filmes/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.ronyelison.filmes.repositories;
+
+import com.ronyelison.filmes.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Optional<Usuario> findByEmail(String email);
+}
